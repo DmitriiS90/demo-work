16c7f73da83b51664fe349b113d5b120d5dab895
DmitriiS90 <shegolev1990@yandex.ru>
2020-11-05 10:20:29 +0300

    Updates

---
+++ precache-manifest.7423443830152151757f74654dd30f8f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6b6f97e66fc7002b810c88f8f6ba4d",
+    "url": "/demo-work/index.html"
+  },
+  {
+    "revision": "8b4d7a455cb1f3acfec0",
+    "url": "/demo-work/static/css/2.00a338ba.chunk.css"
+  },
+  {
+    "revision": "166a391cfdebf39b91f5",
+    "url": "/demo-work/static/css/main.6f04db84.chunk.css"
+  },
+  {
+    "revision": "8b4d7a455cb1f3acfec0",
+    "url": "/demo-work/static/js/2.64b4242e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/demo-work/static/js/2.64b4242e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166a391cfdebf39b91f5",
+    "url": "/demo-work/static/js/main.f7e17162.chunk.js"
+  },
+  {
+    "revision": "362b8634985e8ba93d35",
+    "url": "/demo-work/static/js/runtime-main.b0f1f605.js"
+  },
+  {
+    "revision": "6fb841a4ea48d6d9e793f8afbe0a94c9",
+    "url": "/demo-work/static/media/loader2.6fb841a4.gif"
+  },
+  {
+    "revision": "8793711e8f43b31d9909f117cb1ddedb",
+    "url": "/demo-work/static/media/user2.8793711e.jpg"
+  }
+]);
