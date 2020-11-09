9c1040fdad71a83fec05256dedd695f57bf05760
DmitriiS90 <shegolev1990@yandex.ru>
2020-11-09 11:00:55 +0300

    add news class

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './News.module.css';
+import cn from 'classnames'
 import { Button } from 'antd';
 
 
 const News = () => {
+    const [editMode, setEditMode] = useState(true);
+
     return (
         <div>
-            News
+            <button className={cn({[styles.active]:editMode})} onClick={()=>{setEditMode(true)}}>News</button>
+            <button className={cn({[styles.active]:!editMode})} onClick={()=>{setEditMode(false)}}>Yes</button>
+            <button className={cn({[styles.active]:!editMode})} onClick={()=>{setEditMode(false)}}>No</button>
         </div>
     )
 
